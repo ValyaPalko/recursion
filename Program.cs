54bf822022614cb2154de883d4259239e6b5a678
ValyaPalko <162821115+ValyaPalko@users.noreply.github.com>
2024-04-09 22:47:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+void ArrayBackward(int[] array, int i = 0)
+{
+    if (i < 10)
+    {
+        ArrayBackward(array, i + 1);
+        Console.WriteLine(array[i]);
+    }
+}
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+ArrayBackward(array);
+
+
+
